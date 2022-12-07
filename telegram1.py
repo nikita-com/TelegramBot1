c7e9b8ffc6908408136f4b8e93f6eb23d2710405
Stovpiak NiKiTa <stovpyaknikita@gmail.com>
2022-12-07 19:39:30 +0100

    рш

--- telegram1.py
+++ telegram1.py
@@ -5,7 +5,13 @@ bot = telebot.TeleBot('5803491014:AAGP1Fc4g1k_dcyhYYIL9EReGr6xVLstmpY')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, '<b>привет<b>, ты написал мне /start', parse_mode="HTML")
+    bot.send_message(message.chat.id, 'привет, ты написал мне /start')
 
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Я вас приведствую')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Увидимся')
 
 bot.polling()
