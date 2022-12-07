e27ab0449c5e1d1380db2f771eee2d275469d12d
Stovpiak NiKiTa <stovpyaknikita@gmail.com>
2022-12-07 20:35:59 +0100

    time commit

--- telegram1.py
+++ telegram1.py
@@ -1,17 +1,26 @@
 import telebot
+import datetime
 
 bot = telebot.TeleBot('5803491014:AAGP1Fc4g1k_dcyhYYIL9EReGr6xVLstmpY')
+
+keybord1 = telebot.types.ReplyKeyboardMarkup(True)
+keybord1.row('Привет', 'Пока', 'какое число?', 'сколко времени?')
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'привет, ты написал мне /start', reply_markup=keybord1)
+
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text == 'Привет':
+    if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, 'Я вас приведствую')
-    elif message.text == 'Пока':
+    elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Увидимся')
+    elif message.text.lower() == 'какое число?':
+        bot.send_message(message.chat.id, datetime.date.today().strftime('%y, %b., %d'))
+    elif message.text.lower() == 'сколко времени?':
+        bot.send_message(message.chat.id, datetime.datetime.today().strftime('%H:%M'))
 
 bot.polling()
